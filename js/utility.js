d2d370eaef9f24a4d064d33876e105b9297c8c29
farhan-2204 <farhanauvro@gmail.com>
2024-09-25 04:03:17 +0600

    History section updated, now appends cards upon donation

--- js/utility.js
+++ js/utility.js
@@ -26,6 +26,30 @@ function donate(btn, give, amount){
         document.getElementById(amount).innerText = newBalance;
         document.getElementById('balance').innerText = newBalance1;
 
+        //history section
+        let historyText = _addMoneyInput + ' Taka is Donated for ';
+
+        if(give === 'donate-feni')
+            historyText += 'Flood Relief at Feni, Bangladesh';
+        else if (give === 'donate-noakhali') 
+            historyText += 'Flood Relief at Noakhali, Bangladesh';
+        else if(give === 'donate-quota')
+            historyText += 'Aid for Injured in the Quota Movement, Bangladesh';
+
+        
+        let p = document.createElement('div');
+        let date = new Date();
+
+        p.innerHTML += `<div class="border border-gray-300 rounded-2xl flex flex-col mx-[9vw] mb-6 p-6 gap-6 lg:gap-2">
+            <div>${historyText}</div>
+            <div class="text-[16px] font-semibold">
+                Date: ${date};
+            </div>
+        </div>`
+
+        document.getElementById('history').appendChild(p);
+
+        //modal section
         if(showModal == true)
             document.getElementById('modal').checked = true;
     })
@@ -40,5 +64,10 @@ function toggle(id1, id2, id11, id22){
         document.getElementById(id1).classList.remove('bg-white', 'text-gray-500', 'border-gray-400');
         document.getElementById(id2).classList.add('bg-white', 'text-gray-500', 'border-gray-400');
         document.getElementById(id2).classList.remove('bg-[#B4F461]', 'text-black', 'border-none');
+
+        if(id11 === 'history')
+            document.getElementById('footer').classList.add('hidden');
+        else
+        document.getElementById('footer').classList.remove('hidden');
     })
 }
